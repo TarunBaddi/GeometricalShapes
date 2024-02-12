2ccf8738e2ee4048e00799e779c331aa85e5db28
Tarun <badditarun02@gmail.com>
2024-02-12 11:06:19 +0530

    Rename class from Main to  ShapeCalculatorr

---
+++ ShapeCalculator.java
@@ -0,0 +1,30 @@
+
+public class ShapeCalculator {
+    public static double TriangleArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+
+    public static double RectangleArea(double length, double width) {
+        return length * width;
+    }
+
+    public static double CircleArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double SquareArea(double side) {
+        return Math.pow(side, 2);
+    }
+
+    public static void main(String[] args){
+        double triangleArea = TriangleArea(5,5);
+        double rectangleArea = RectangleArea(8,8);
+        double circleArea = CircleArea(7);
+        double squareArea = SquareArea(4);
+
+        System.out.println("Triangle Area: " + triangleArea);
+        System.out.println("Rectangle Area: " + rectangleArea);
+        System.out.println("Circle Area: " + circleArea);
+        System.out.println("Square Area: " + squareArea);
+    }
+}
